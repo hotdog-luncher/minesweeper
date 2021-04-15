582fafdeee9a60f7433dc882c96d2814ffd955ad
Brandon Saimo <n9643750@hotmail.com>
2021-04-15 13:36:05 -0700

    added documentation

--- gameboard.py
+++ gameboard.py
@@ -6,27 +6,23 @@ class Game_board:
         self.num_columns = columns
         self.num_mines = mines
         self.is_first_turn = True
-        self.mine_field = []
-        self.mine_set = set()
-
-        #creates an array of size num_rows x num_columns full of '0's
-        for row in range(self.num_rows):
+        self.mine_field = []                        # holds the master game_board with mines and numbers
+        self.mine_set = set()                       # coordinates of all mines
+    
+        for row in range(self.num_rows):            #creates an array of size num_rows x num_columns full of '0's
             self.mine_field.append([])
             for column in range(self.num_columns):
                 self.mine_field[row].append(0)
-
-    #populate the board with mines excluding tiles touching first choice
-    def place_mines(self, x, y):  
+ 
+    def place_mines(self, x, y):                    #populate the board with mines excluding tiles touching first choice
         mine_counter = self.num_mines
-
-        #switch values of tiles around first choice from zero to X
-        for row in range(x-1, x+2):
+      
+        for row in range(x-1, x+2):                 #switch values of tiles around first choice from zero to X
             for column in range(y-1, y+2):
                 if row in range(0, self.num_rows) and column in range(0, self.num_columns):  
                     self.mine_field[row][column] = 'X'
-        
-        #add specified number of mines to board in random tiles excluding x'd out tiles
-        while mine_counter != 0:
+              
+        while mine_counter != 0:                     #add specified number of mines to board in random tiles excluding x'd out tiles
             rand_row = random.randrange(0, self.num_rows)
             rand_col = random.randrange(0, self.num_columns)
             if self.mine_field[rand_row][rand_col] == 0:
@@ -34,7 +30,7 @@ class Game_board:
                 self.mine_set.add((rand_row, rand_col))
                 mine_counter -= 1
 
-    def fill_board(self):
+    def fill_board(self):                           #populate the board with numbers for mines touching each tile
         for x in range(self.num_rows):
             for y in range(self.num_columns):             
                 if self.mine_field[x][y] != '*':          #iterate through each tile that is not a mine
