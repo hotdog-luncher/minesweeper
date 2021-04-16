35d660eb1ea54a6718ac3e3bc78055db0c3ac27b
Brandon Saimo <n9643750@hotmail.com>
2021-04-15 19:34:01 -0700

    finished writing methods

--- minesweeper.py
+++ minesweeper.py
@@ -4,25 +4,27 @@ from userboard import User_board
 
 #testing
 
-x = Game_board(4,4, 3)
+x = Game_board(10, 10, 10)
 x.place_mines(4,7)
-#print(x.mine_field)
-
-#x.fill_board()
-#print(x.mine_field)
+x.fill_board()
 
 y = User_board(x)
 
 #row, column = y.get_coords()
 #print(row, column)
 #y.add_mines()
+#y.display_board()
+#y.explode()
+
+#y.unflag()
+#y.unflag()
+#y.unflag()
+#y.explode()
+y.open_tile(2,2)
+y.open_zeros(2,2)
 y.display_board()
-#y.explode()
-#y.place_flag()
-#y.place_flag()
-#y.place_flag()
-#y.unflag()
-#y.unflag()
-y.unflag()
-#y.explode()
+y.place_flag()
+y.place_flag()
+y.place_flag()
+y.double_click()
 y.display_board()
--- userboard.py
+++ userboard.py
@@ -14,7 +14,7 @@ class User_board:
             for column in range(self.num_columns):
                 self.user_array[row].append('-')
 
-    def get_coords(self):                          #get row and column coordinates and validate input, then return them          
+    def get_coords(self):                       #get row and column coordinates and validate input, then return them          
         while True:                                                                    #get row
             while True:
                 try:
@@ -43,7 +43,7 @@ class User_board:
 
         return row - 1, column - 1
 
-    def display_board(self):                       #display user board, including row and column numbers, opened tiles, dashes, and flags
+    def display_board(self):                    #display user board, including row and column numbers, opened tiles, dashes, and flags
         x = 1
         print('  ', end= ' ')
         for num in range(1, self.num_columns+1):          
@@ -60,13 +60,13 @@ class User_board:
                 print(self.user_array[row][column], end='  ')
         print()
 
-    def add_mines(self):                           #add mines from the game board to the user board
+    def add_mines(self):                        #add mines from the game board to the user board
         for coord in self.mine_set:
             x = coord[0]
             y = coord[1]
             self.user_array[x][y] = '*'
 
-    def explode(self):                             #call add mines, check for bad flags and display the board
+    def explode(self):                          #call add mines, check for bad flags and display the board
         self.add_mines()
         bad_flag = self.flag_set.difference(self.mine_set)
         
@@ -81,18 +81,20 @@ class User_board:
         while new_game not in ['y', 'n']:
             new_game = input("you lose! would you like to play again? (y/n)").lower()     
         
-        self.play_game = new_game
+        self.play_game
             
-    def place_flag(self):                          #allows user to place flag representing a mine
+    def place_flag(self):                       #allows user to place flag representing a mine
         print("Place a flag to mark a suspected mine")
         row, column = self.get_coords() 
 
         if self.user_array[row][column] == 'F':
             print("This tile is already flagged \n")
+            self.display_board()
             return
         
         elif self.user_array[row][column] != '-':
             print("This tile has already been opened \n")
+            self.display_board()
             return
         
         else:
@@ -101,7 +103,7 @@ class User_board:
         
         self.display_board()
 
-    def unflag(self):                              #allows user to remove a previously placed flag
+    def unflag(self):                           #allows user to remove a previously placed flag
         if self.flag_set:
             print("Remove a flag from the board")
             row, column = self.get_coords()
@@ -113,7 +115,89 @@ class User_board:
         else:
             print("There are currently no flags\n")
         self.display_board()
+
+    def open_tile(self, x, y):                  #sets user array equal to minefield at a given tile
+        self.user_array[x][y] = self.mine_field[x][y]
+
+    def get_unopened_neighbors(self, x, y):     #takes in coords and builds a set of tile coords for tiles that are neighbors, in range and unopened ('-' on userboard) and returns it
+        unopened_set = set()
+
+        for row in range(x-1, x+2):                 
+            for column in range(y-1, y+2):
+                if row in range(0, self.num_rows) and column in range(0, self.num_columns):  
+                    if self.user_array[row][column] == '-':
+                        unopened_set.add((row,column))
+        
+        return unopened_set
+
+    def open_zeros(self, x, y):                 #opens each tile that is a zero, a neighbor and not a bomb and repeats action for each adjacent tile
+        if self.user_array[x][y] != 0:
+            return
+        
+        else:
+            unopened_neighbor_set = self.get_unopened_neighbors(x,y)
+            while unopened_neighbor_set:
+                coords = unopened_neighbor_set.pop()
+                self.open_tile(coords[0],coords[1])
+                if self.mine_field[coords[0]][coords[1]] == 0:
+                    unopened_neighbor_set = unopened_neighbor_set.union(self.get_unopened_neighbors(coords[0],coords[1]))
+
+    def double_click(self):                     #gets coords, if tile is open, and tile value == the number of neighbor tiles with mines open all neighbor tiles and call open zeros on them 
+        x, y = self.get_coords()
+        tile_value = self.user_array[x][y]
+        flag_count = 0
+
+        if tile_value == '-':
+            print('Tile is not yet opened\n')
+            return
+        
+        for row in range(x-1, x+2):                    #for each tile iterate through all the tiles around it counting flags
+             for column in range(y-1, y+2):
+                if row in range(0, self.num_rows) and column in range(0, self.num_columns):  #if tile is on board
+                    if self.user_array[row][column] == 'F':
+                        flag_count += 1
+        
+        if flag_count > tile_value:
+            print("too many flags around tile\n")
+
+        elif tile_value > flag_count:
+            print("not enough flags around tile\n") 
+
+        else:
+            for row in range(x-1, x+2):                  #for each tile iterate through all the tiles around it 
+             for column in range(y-1, y+2):
+                if row in range(0, self.num_rows) and column in range(0, self.num_columns):  #if tile is on board
+                    if self.user_array[row][column] == '-':                                  #open tile, if bomb explode
+                        if self.mine_field[row][column] == '*':
+                            self.explode()
+                        self.user_array[row][column] = self.mine_field[row][column]
+                        self.open_zeros(row, column)
+
+    def choose(self):                           #gets coords, if bomb explode, if '-' open tile and call open zeros
+        x, y = get_coords(self):
+        
+        if self.user_array[x][y] == 'F':
+            print("This tile is flagged as a mine and cannot be selected\n")
+            self.display_board()
+            return
+        
+        if self.user_array[x][y] != '-':
+            print("This tile has already been opened\n")
+            self.display_board()
+            return
+        
+        if self.mine_field[x][y] == '*':
+            self.explode()
+            return
+
+        self.user_array[x][y] = self.mine_field[x][y]
+        self.open_zeros(x,y)
+        self.display_board()
         
 
+        
+        
+
+        
 
 
